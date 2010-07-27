a6c29bd3869b3a122c14165713b8ea76c5b94eef
K.S. Wong <admin@kswong.co.cc>
2010-07-27 20:55:58 +0800

    added rain theme

--- js/jquery.scrollbg.js
+++ js/jquery.scrollbg.js
@@ -2,19 +2,26 @@
 
 (function($) {
 	$.fn.scrollBg = function(options) {
-		var opts = $.extend({}, $.fn.scrollBg.defaults, options);
-		var deltaPx = Math.ceil(opts.speed*opts.interval/1000);
-		var current = 0;		
+		var opts = $.extend(true, {}, $.fn.scrollBg.defaults, options);
+		var deltaX = Math.ceil(opts.speed.x*opts.interval/1000);
+		var deltaY = Math.ceil(opts.speed.y*opts.interval/1000);
+		var currentX = 0, currentY = 0;		
 		var target = this;
 
 		this.css("background-repeat","repeat");
 
 		function scroll(){
-			current -= deltaPx;
-			if (current <= -opts.imageHeight){
-				current += opts.imageHeight;
+			currentX += deltaX;
+			currentY += deltaY;
+			if (currentX <= -opts.imageSize.x || currentX >= opts.imageSize.x){
+				currentX += opts.imageSize.x;
+				currentX %= opts.imageSize.x;
 			}
-			target.css("background-position","0 "+current+"px");		
+			if (currentY <= -opts.imageSize.y || currentY >= opts.imageSize.y){
+				currentY += opts.imageSize.y;
+				currentY %= opts.imageSize.y;
+			}
+			target.css("background-position",currentX+"px "+currentY+"px");		
 		}
 
 		setInterval(scroll, opts.interval);
@@ -23,8 +30,8 @@
 	}
 
 	$.fn.scrollBg.defaults  = {
-		speed: 10,
+		speed: {x: 0, y: 0},
 		interval: 250,
-		imageHeight: 0
+		imageSize: {x: 0, y: 0}
 	}
 })(jQuery);
--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 // Main script
 
 jQuery(document).ready(function($) {
-	$('body').scrollBg({imageHeight: 300});
+	$.getJSON('http://query.yahooapis.com/v1/public/yql', {format: 'json', q: 'select * from weather.forecast where location in ("CHXX0049") and u="c"'},
+		function(data) {
+ 			var pattern = /(storm|rain|shower)/i;
+			var weather = data.query.results.channel.item.condition.text;
+			var result = weather.match(pattern);
+			
+			if (result != null) {
+				$('body').addClass("rain").scrollBg({imageSize: {x: 108, y: 108}, speed: {x:10, y: 20}});
+			} else {
+				$('body').scrollBg({imageSize: {x: 300, y: 300}, speed: {y: -15}});
+			}
+		}
+	);
+	
 	$('#planeBg').height($(document).height()).plane();
 });
 
